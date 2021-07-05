80a7455396cc238b40f12fd2fba4549aaa607082
Tony Benoy <me@tonybenoy.com>
2021-07-05 19:59:34 +0530

    chapter 7 partial

---
+++ packaging/src/lib.rs
@@ -0,0 +1,23 @@
+mod hotel {
+    pub mod room {
+        pub fn check_available_room() {}
+        pub fn book_room() {}
+        pub fn unbook_room() {}
+    }
+    mod restaurant {
+        fn take_order() {}
+        fn pay_bill() {}
+    }
+}
+
+pub fn get_room() {
+    crate::hotel::room::check_available_room();
+    hotel::room::book_room();
+}
+
+use crate::hotel::room;
+
+pub fn remove_room(){
+    room::unbook_room()
+    room::check_available_room()
+}
